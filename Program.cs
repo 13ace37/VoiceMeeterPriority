161248848309d6bccf2c7f010beb3fad2d911876
ace <ace@HOME>
2022-09-01 20:18:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace VoiceMeeterPriority
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Process audiodg = Process.GetProcessesByName("audiodg")[0];
+            audiodg.ProcessorAffinity = (IntPtr)(1L << 1);
+            audiodg.PriorityClass = ProcessPriorityClass.High;
+            foreach (ProcessThread thread in audiodg.Threads)
+                thread.ProcessorAffinity = (IntPtr)(1L << 1);
+
+        }
+    }
+}
